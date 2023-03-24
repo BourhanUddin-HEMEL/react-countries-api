9e27cc5a4bc6996f14a5b91c0416e10793063582
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-03-25 01:00:23 +0600

    finals

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
-    useState(()=> {
+    useEffect(()=> {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data =>{
